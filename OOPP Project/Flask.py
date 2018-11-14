3bca9f0842e5213f5528565567b6ce9295454d08
leemuel01 <44252731+leemuel01@users.noreply.github.com>
2018-11-14 12:41:53 +0800

    Add files via upload

--- OOPP Project/Flask.py
+++ OOPP Project/Flask.py
@@ -15,11 +15,30 @@ def profile():
     student1 = Student("Test", "Male", "12345X")
     # student1.set_mark("70")
 
-
     if request.method == "POST":
         results = request.form
 
     return render_template("info.html",  result=results, student = student1)
+
+@app.route("/templates/Symptom Checker.html", methods = ['POST', 'GET'])
+def check():
+    title = "Symptom Checker"
+    return render_template("Symptom Checker.html", title=title )
+
+@app.route("/templates/trivia.html", methods = ['POST', 'GET'])
+def trivia():
+    title = "Medical Trivia"
+    return render_template("trivia.html", title=title )
+
+@app.route("/templates/medical teacher.html", methods = ['POST', 'GET'])
+def teacher():
+    title = "Medical Tutor  "
+    return render_template("medical teacher.html", title=title )
+
+@app.route("/templates/feedback.html", methods = ['POST', 'GET'])
+def feedback():
+    title = "Feedback"
+    return render_template("feedback.html", title=title )
 
 if __name__ == "__main__":
     app.run(debug=True)
