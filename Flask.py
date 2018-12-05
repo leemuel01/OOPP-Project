633284a5d5545989f51d146a5db1cab438d6814b
Ryuse <leemuel01234@gmail.com>
2018-12-05 16:11:35 +0800

    Validation stuff and database is gone for a bit

--- Flask.py
+++ Flask.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, flash, url_for, redirect
 from flask_mysqldb import MySQL
-import yaml
-from Feedback import Feedback
+
+from Forms import Registration_Form, Login_Form   #Form classes
+import yaml      #Database login and stuff
 
 app = Flask(__name__)
+
+#Security stuff so no cookie configuration
+app.config["SECRET_KEY"] = '49dc263f9ebdbf0510c9273e5320e7df'
 
 #Configure database
 db = yaml.load(open("db.yaml"))
@@ -17,31 +21,47 @@ mysql = MySQL(app)
 #Flask pages
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    title = "Home"
+    form = Registration_Form()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        # return redirect(url_for("index"))
 
-    if request.method == "POST":
-        #Fetch the form data
-        results = request.form
-        username = results["name"]
-        password = results["pw"]
-        email = results["email"]
-        fullname = results["fullname"]
-        nric = results["nric"]
+    # if request.method == "POST":
+    #     #Fetch the form data
+    #     results = request.form
+    #     username = results["name"]
+    #     password = results["pw"]
+    #     email = results["email"]
+    #     fullname = results["fullname"]
+    #     nric = results["nric"]
+    #
+    #     # return 'success'
+    #
+    #     cur = mysql.connection.cursor()
+    #     cur.execute("INSERT INTO Users(Username, Password, Email, Full_Name, NRIC) VALUES(%s, %s, %s, %s, %s)", (username, password, email, fullname, nric))
+    #     mysql.connection.commit()
+    #     cur.close()
 
-        # return 'success'
 
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO Users(Username, Password, Email, Full_Name, NRIC) VALUES(%s, %s, %s, %s, %s)", (username, password, email, fullname, nric))
-        mysql.connection.commit()
-        cur.close()
 
-    return render_template("home.html", title=title )
+    return render_template("home.html", title="Home", form=form )
 
-@app.route("/results", methods = ['POST', 'GET'])
+@app.route("/templates/register.html", methods = ['POST', 'GET'])
 
-# def profile():
-#
-#     return render_template("info.html",  result=results)
+def register():
+
+    form = Registration_Form()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for("home"))
+
+    return render_template("register.html", title="Sign Up", form=form)
+
+@app.route("/login", methods = ['POST', 'GET'])
+def login():
+    form = Login_Form()
+
+    return render_template("login.html",  title="Login", form=form)
 
 
 
@@ -56,13 +76,16 @@ def trivia():
     title = "Medical Trivia"
     return render_template("trivia.html", title=title )
 
+#====================================== Medical Teacher Page =========================================
+
 @app.route("/templates/medical teacher.html", methods = ['POST', 'GET'])
 def teacher():
     title = "Medical Tutor  "
     return render_template("medical teacher.html", title=title )
 
+#====================================== Feedback Page =========================================
 
-
+from Feedback import Feedback
 @app.route("/templates/feedback.html", methods = ['POST', 'GET'])
 def feedback():
     title = "Feedback"
