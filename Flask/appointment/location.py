00d2e0edfa5760d1d46679a937b7a57df18e9fd4
Hexnode <41111944+Hexnode@users.noreply.github.com>
2019-01-12 01:16:31 +0800

    Update location.py
    opening hours

--- Flask/appointment/location.py
+++ Flask/appointment/location.py
@@ -1,20 +1,34 @@
 import requests
 
+# user inputs location #
 user_location_input = "singapore+400322"
+# my google api key #
 googlemaps_api_key = "AIzaSyCWLbnx9H1I-HnwzHh9kbL8PyZvYGJydiQ"
+# requesting location of user input #
 googlemaps_req = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address="+user_location_input+"&key="+googlemaps_api_key+"")
+# debug show location results#
 print(googlemaps_req.json()["results"][0]["geometry"]["location"])
 
-googlemaps_req_latitude = googlemaps_req.json()['results'][0]["geometry"]["location"]["lat"]
-googlemaps_req_longitude = googlemaps_req.json()['results'][0]["geometry"]["location"]["lng"]
+# user input latitude results#
+googlemaps_req_latitude = str(googlemaps_req.json()['results'][0]["geometry"]["location"]["lat"])
+# user input longitude results#
+googlemaps_req_longitude = str(googlemaps_req.json()['results'][0]["geometry"]["location"]["lng"])
+# using the previous location latitude and longitude to get nearby hospitals ranked by distance from location input#
+googlemaps_req_nearby_hospital = requests.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+googlemaps_req_latitude+","
+                                              +googlemaps_req_longitude+"&rankby=distance&type=hospital&keyword=general+hospital&key="+googlemaps_api_key+"")
+# debug show results#
+print(googlemaps_req_nearby_hospital.json()["results"][1])
 
-googlemaps_req_nearby_hospital = requests.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+str(googlemaps_req_latitude)+","+str(googlemaps_req_longitude)+"&radius=15000&type=hospital&keyword=general+hospital&key="+googlemaps_api_key+"")
-
-print(googlemaps_req_nearby_hospital.json()["results"][0])
-
+# loop to print results#
 simple_count = 0
 for i in googlemaps_req_nearby_hospital.json()["results"]:
     simple_count+=1
-    print(simple_count, i["name"])
+    print(simple_count, i["name"], "is currently: ",end="")
+    if "opening_hours" in i and i["opening_hours"]["open_now"] == True:
+        print("open")
+    elif "opening_hours" in i and i["opening_hours"]["open_now"] == False:
+        print("closed")
+    else:
+        print("no opening hours data available")
 
 simple_count = 0
