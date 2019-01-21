3cfd7cf8699dd6836e565d7e37db1b4543239397
Lim Han Loon Dominic <181513c@mymail.nyp.edu.sg>
2019-01-21 23:06:10 +0800

    update
    added basics of sqlalchemy

--- Flask/symptom_checker/routes.py
+++ Flask/symptom_checker/routes.py
@@ -1,7 +1,29 @@
 from flask import flash, redirect, url_for, render_template, Blueprint
 from Flask.symptom_checker.forms import Symptom_Checker_Form
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///symptom.db'
+db = SQLAlchemy(app)
+
+
+class SymptomAnswer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    symptom_answer = db.Column(nullable=False)
+
+    def __repr__(self):
+        return f"{self.symptom_answer}"
+
+
+class SymptomTreatment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    symptom_treatment = db.Column(nullable=False)
+
+    def __repr__(self):
+        return f"{self.symptom_treatment}"
+
 
 symptom_checker = Blueprint('symptom_checker', __name__)
+
 
 @symptom_checker.route("/Symptom Checker", methods = ['POST', 'GET'])
 def symptom_check():
