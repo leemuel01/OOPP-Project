a9acdede6391c12c1545ce594b3bdef15fb286fa
joiulll12 <44657794+joiulll12@users.noreply.github.com>
2019-01-22 19:21:52 +0800

    Add files via upload

---
+++ OOPP-Project-v2/Flask/main/routes.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from flask import Blueprint, render_template, flash
+from Flask import db
+from Flask.Models import User
+
+main = Blueprint('main', __name__)
+
+
+@main.route("/", methods=['GET', 'POST'])
+def index():
+
+    db.create_all()
+
+    expired_users = User.query.filter(User.delete_time<=datetime.utcnow())
+    for i in expired_users:
+        if i.authenticated == False:
+            db.session.delete(i)
+            db.session.commit()
+
+    return render_template("home.html")
