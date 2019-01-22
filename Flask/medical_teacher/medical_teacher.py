42472750266d1c37a40761e3f3306806494111a3
yuhongnoob <44657945+yuhongnoob@users.noreply.github.com>
2019-01-23 01:50:22 +0800

    morething

--- Flask/medical_teacher/medical_teacher.py
+++ Flask/medical_teacher/medical_teacher.py
@@ -13,30 +13,28 @@ print(review_display)
 for i in review_display:
     print("id= ", i[0], i[1], i[2])
 """
+class MedicalTeacher():
+    def __init__(self):
+        pass
 
-def display_review():
-    sqlite_connection = sqlite3.connect("Flask/site.db")
-    row_count = sqlite_connection.execute("select count(*) from post_review").fetchone()[0]
-    print(row_count)
-    all_review = []
-    for i in range(row_count):
-        review_row = sqlite_connection.execute("select * from post_review where id = ?", (i+1,))
+    def display_review(self):
+        sqlite_connection = sqlite3.connect("Flask/site.db")
+        row_count = sqlite_connection.execute("select count(*) from post_review").fetchone()[0]
+        print(row_count)
+        all_review = []
+        for i in range(row_count):
+            review_row = sqlite_connection.execute("select * from post_review where id = ?", (i+1,))
 
-        for j in review_row:
-            row_review = [j[0], j[1], j[2]]
-            all_review.append(row_review)
-    return all_review
+            for j in review_row:
+                row_review = [j[0], j[1], j[2]]
+                all_review.append(row_review)
+        return all_review
 
 
-display_review()
 
 
 #=============================================post=========================================
-def submit():
-    variable1 = requests.form.get("project")
-    form_date_now = datetime.datetime.now()
-    post_comment = review_display.execute("insert into review_table_name(content,date_posted) values (?,?)"
-                                            , (variable1, form_date_now))
+
 
 
 
--- Flask/medical_teacher/routes.py
+++ Flask/medical_teacher/routes.py
@@ -1,20 +1,27 @@
-from flask import Blueprint, render_template
-from Flask.medical_teacher.medical_teacher import display_review
-import sqlite3, request, datetime
+from flask import Blueprint, render_template, request
+from Flask.medical_teacher.medical_teacher import MedicalTeacher
+import sqlite3, datetime
 
 medical_teacher = Blueprint('medical_teacher', __name__)
+
+
 
 @medical_teacher.route("/Medical Teacher", methods = ['POST', 'GET'])
 def teacher():
     title = "Medical Tutor "
-    return render_template("Medical Teacher/medical teacher.html", title=title, display_review=display_review())
+    health = MedicalTeacher()
+    return render_template("Medical Teacher/medical teacher.html", title=title, display_review=health.display_review())
 
 
 @medical_teacher.route('/handle_data', methods=['POST'])
 def handle_data():
     sqlite_connection = sqlite3.connect("Flask/site.db")
+    health = MedicalTeacher()
     review_display = sqlite_connection.execute("select * from post_review where id=2")
     variable1 = request.form.get("project")
     form_date_now = datetime.datetime.now()
-    post_comment = sqlite_connection.execute("insert into review_table_name(content,date_posted) values (?,?)"
+    post_comment = sqlite_connection.execute("insert into post_review(content,date_posted) values (?,?)"
                                           , (variable1, form_date_now))
+    sqlite_connection.commit()
+    return render_template("Medical Teacher/handle_data.html",display_var=variable1
+                           , display_review=health.display_review())
