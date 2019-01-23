40e8f7e13d9f071815bcb6d7abefc3ebc33a4e50
Lim Han Loon Dominic <181513c@mymail.nyp.edu.sg>
2019-01-23 15:08:31 +0800

    update
    using sql now

--- Flask/symptom_checker/routes.py
+++ Flask/symptom_checker/routes.py
@@ -3,7 +3,6 @@ from Flask.symptom_checker.forms import Symptom_Checker_Form
 import sqlite3
 
 symptom_checker = Blueprint('symptom_checker', __name__)
-symptom_database = sqlite3.connect("../site.db")
 
 
 @symptom_checker.route("/Symptom_Checker", methods=['POST', 'GET'])
@@ -19,17 +18,23 @@ def symptom_check():
 
 @symptom_checker.route("/symptom", methods=['POST', 'GET'])
 def symptom():
-
+    symptom_database = sqlite3.connect("flask/site.db")
     symptom_checked_box = request.form.get("answer")
-    return render_template("Symptom Checker/Symptom.html", title="Symptom Checker",test3=symptom_checked_box)
+    data_symptom_answer = symptom_database.execute('SELECT symptom_answer FROM symptom WHERE id=?',(symptom_checked_box))
+    for i in data_symptom_answer:
+        data_symptom_answer = i[0]
+    data_symptom_treatment = symptom_database.execute('SELECT symptom_treatment FROM symptom WHERE id=?',(symptom_checked_box))
+    for r in data_symptom_treatment:
+        data_symptom_treatment = r[0]
+    return render_template("Symptom Checker/Symptom.html", title="Symptom Checker", test3=data_symptom_answer,
+                           test4=data_symptom_treatment)
 
 # <--- To take the data and put it at the symptom part at Symptom.html --->
 # def select_answer:
-#     symptom_database.execute('SELECT symptom_answer FROM symptom')
-#     data_symptom_answer = symptom_database.fetchall()
-#     print(data_symptom_answer)
+#     symptom_database.execute('SELECT symptom_answer FROM symptom WHERE id=1')
+#     data_symptom_answer = symptom_database.fetchone()
+#     symptom_database.execute('SELECT symptom_treatment FROM symptom WHERE id=1')
+#     data_symptom_treatment = symptom_database.fetchone()
 
 # <--- To take the data and put it at the how to treat your symptom part at Symptom.html --->
-#     symptom_database.execute('SELECT symptom_treatment FROM symptom')
-#     data_symptom_treatment = symptom_database.fetchall()
-#     print(data_symptom_treatment)
+
