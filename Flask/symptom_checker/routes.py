2d03a539ed5adab8517de796898bbb1a8cb394fe
Lim Han Loon Dominic <181513c@mymail.nyp.edu.sg>
2019-01-23 13:32:46 +0800

    update

--- Flask/symptom_checker/routes.py
+++ Flask/symptom_checker/routes.py
@@ -1,25 +1,28 @@
-from flask import flash, redirect, url_for, render_template, Blueprint
+from flask import flash, redirect, url_for, render_template, Blueprint, request
 from Flask.symptom_checker.forms import Symptom_Checker_Form
+import sqlite3
 
 symptom_checker = Blueprint('symptom_checker', __name__)
+symptom_database = sqlite3.connect("../site.db")
 
 
 @symptom_checker.route("/Symptom Checker", methods = ['POST', 'GET'])
 def symptom_check():
     form = Symptom_Checker_Form()
     if form.validate_on_submit():
-        flash("banana", 'success')
-        return redirect(url_for('main.index'))
+        symptom_checked_box = request.form.get("answers")
+        print(symptom_checked_box)
+        # return redirect(url_for('main.index'))
     return render_template("Symptom Checker/Symptom Checker.html", title="Symptom Checker", form=form)
 
 
 # <--- To take the data and put it at the symptom part at Symptom.html --->
 # def select_answer:
-#     <variable>.execute('SELECT symptom_answer FROM symptom')
-#     data_symptom_answer = <variable>.fetchall()
+#     symptom_database.execute('SELECT symptom_answer FROM symptom')
+#     data_symptom_answer = symptom_database.fetchall()
 #     print(data_symptom_answer)
 
 # <--- To take the data and put it at the how to treat your symptom part at Symptom.html --->
-#     <variable>.execute('SELECT symptom_treatment FROM symptom')
-#     data_symptom_treatment = <variable>.fetchall()
+#     symptom_database.execute('SELECT symptom_treatment FROM symptom')
+#     data_symptom_treatment = symptom_database.fetchall()
 #     print(data_symptom_treatment)
