16e443226bf582f67d116ff25658c6f9aa4a9ce5
Ryuse <leemuel01234@gmail.com>
2019-01-28 19:34:30 +0800

    Minor changes

--- Flask/medical_teacher/routes.py
+++ Flask/medical_teacher/routes.py
@@ -5,4 +5,4 @@ medical_teacher = Blueprint('medical_teacher', __name__)
 @medical_teacher.route("/Medical Teacher", methods = ['POST', 'GET'])
 def teacher():
     title = "Medical Tutor  "
-    return render_template("Medical Teacehr/medical teacher.html", title=title )
+    return render_template("Medical Teacher/medical teacher.html", title=title )
